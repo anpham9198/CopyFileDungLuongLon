6aa671685d70e7f58d914384b3a749d8f19b99c4
anpham9198 <anpham9198@gmail.com>
2024-11-25 21:18:30 +0700

    Add project files.

---
+++ CopyFileDungLuongLon/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter source file");
+        string sourcePath = Console.ReadLine();
+
+        Console.WriteLine("Enter file Destination");
+        string destinationPath = Console.ReadLine();
+
+        FileInfo source = new FileInfo(sourcePath);
+        FileInfo destination = new FileInfo(destinationPath);
+        try
+        {
+            CopyFileUsingFileInfor(source,destination);
+            Console.WriteLine("File copy");
+        }
+        catch (System.Exception e)
+        {
+            Console.WriteLine("Not copy");
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+
+    private static void CopyFileUsingFileInfor(FileInfo source, FileInfo destination)
+    {
+        source.CopyTo(destination.FullName, true);
+    }
+}
